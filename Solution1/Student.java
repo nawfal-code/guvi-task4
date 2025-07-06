e70baf1d747164714bfad6452ccb5692144e4171
nawfal-code <nawfal.mohammed006@gmail.com>
2025-07-06 17:10:43 +0530

    Create Student.java

---
+++ Solution1/Student.java
@@ -0,0 +1,15 @@
+package guvi.task4;
+//class students with attributes
+public class Student {
+    String rollno;
+    String name;
+    int age;
+    String course;
+    //parameterized constructor
+    public Student(String rollno,String name,int age,String course){
+        this.rollno=rollno;
+        this.name=name;
+        this.age=age;
+        this.course=course;
+    }
+}
